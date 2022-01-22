919134701e79664121ccd90fdcf1a426d4477855
Yu134y <y.onosaka134@gmail.com>
2022-01-22 18:53:49 +0900

    add:viewsの実装

--- django/portfolio/app/portfolio/views.py
+++ django/portfolio/app/portfolio/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Profile, Skill, Work
 
 def index(request):
-    context = {}
+    profile = Profile.objects.all().last()
+    skills = Skill.objects.all()
+    works = Work.objects.all().order_by('-published')[:3]
+
+    context = {
+        'profile': profile,
+        'skills': skills,
+        'works': works
+    }
     return render(request, 'index.html', context)
