e473768494a8e3c9fe5ba9ee985e71b790a28bbd
Yu134y <y.onosaka134@gmail.com>
2022-01-24 12:33:48 +0900

    fix:フォルダ構成

--- django/nothpy/app/article/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name= 'article'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('articles', views.article_list, name='article_list'),
-    path('articles/<uuid:uuid>', views.article_detail, name='article_detail'),
-]
