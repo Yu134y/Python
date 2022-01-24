11c807b90013768e0f21d9e130e1e3d4ac90e290
Yu134y <y.onosaka134@gmail.com>
2022-01-24 17:04:27 +0900

    add:ログイン機能

--- django/just_python/mypytodo/app/app/urls.py
+++ django/just_python/mypytodo/app/app/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # accountのurls.pyを読み込み
-    # path('', include('account.urls')),
+    path('', include('account.urls')),
     # todoのurls.pyを読み込み
     path('', include('todo.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
