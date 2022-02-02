d1483ae051edc5d550777cead46b6f86d5194a12
Yu134y <y.onosaka134@gmail.com>
2022-02-02 13:59:05 +0900

    add:form file

--- django/exsports/django_crud/account/views.py
+++ django/exsports/django_crud/account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'account/signup.html', {'form': form})
