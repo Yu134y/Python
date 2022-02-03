487fb6752bbbd9a820f25745d3c963b8862f2b49
Yu134y <y.onosaka134@gmail.com>
2022-02-04 00:43:19 +0900

    add:templates file

--- django/exsports/ec/config/settings.py
+++ django/exsports/ec/config/settings.py
@@ -118,12 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
-
-LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'login'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
