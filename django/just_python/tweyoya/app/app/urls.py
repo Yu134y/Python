c08d75b3bc1e2c33221dff26e61faf9b7a499444
Yu134y <y.onosaka134@gmail.com>
2022-02-06 19:06:11 +0900

    add:update tweyoya

--- django/just_python/tweyoya/app/app/urls.py
+++ django/just_python/tweyoya/app/app/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('twitter.urls')),
-    # path('', include('account.urls')),
+    path('', include('account.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
